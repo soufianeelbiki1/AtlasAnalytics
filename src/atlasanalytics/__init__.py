bf7efaba216555c1342177062bd7678de5c8b932
Soufiane Elbiki <soufiane.elbiki@gmail.com>
2026-08-31 00:31:51 +0100

    feat: export measured executive summary API

--- src/atlasanalytics/__init__.py
+++ src/atlasanalytics/__init__.py
@@ -1,6 +1,13 @@
 """AtlasAnalytics payments warehouse package."""
 
+from .summary import ExecutiveSummary, build_executive_summary
 from .synthetic import SyntheticDataset, generate_synthetic_dataset
 from .warehouse import build_warehouse
 
-__all__ = ["SyntheticDataset", "build_warehouse", "generate_synthetic_dataset"]
+__all__ = [
+    "ExecutiveSummary",
+    "SyntheticDataset",
+    "build_executive_summary",
+    "build_warehouse",
+    "generate_synthetic_dataset",
+]
